350d9c15b91ba9f499849123ef635c1dc400c370
creed cortex <rick05933@gmail.com>
2022-04-28 10:11:48 +0200

    Initializing Django Repo

---
+++ Django/TradingBot/trading_B/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import apii.data as dat
+import apii.plotss as pltt
+
+
+def index(request):
+    chart = pltt.get_plot(dat.balance, 3)
+    return render(request, 'trading_B/hello.html', {'chart': chart})
