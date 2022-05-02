78665f7c180f22fa43b8d1fed8b68c57e4999bf7
Meyer, Niklas (BWI 4) <niklas.meyer.3@student.fhws.de>
2022-05-02 17:27:57 +0200

    Kpi erweitert

--- JavaBackend/Signale/de/fhws/Softwareprojekt/Kpi.java
+++ JavaBackend/Signale/de/fhws/Softwareprojekt/Kpi.java
@@ -4,16 +4,21 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Kpi {
+	JsonCandlesRoot root;
 	public String instrument;
 	public String granularity;
 	public int periods;
+	double atr=0;
+	ArrayList<Double> atrListe=new ArrayList<>();
 	double ema = 0;
 	double vorema=0;
+	double Vormacd=0;
 	double macd=0;
+	double VormacdTriggert=0;
 	double macdTriggert=0;
 	//Signalinie unterhalb macd:Nur relevant für die letzten zwei Durchläufe
-	ArrayList<Double> macds=new ArrayList<>();
-	ArrayList<Double> signals=new ArrayList<>();
+	//ArrayList<Double> macds=new ArrayList<>();
+	//ArrayList<Double> signals=new ArrayList<>();
 	boolean trendWechsel=false;
 double rsi=0;
 	
