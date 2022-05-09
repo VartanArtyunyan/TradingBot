8d8f01e6f9961fbe918ad6d346f467cdad77925e
Heyder, Tim (BWI 4) <tim.heyder@student.fhws.de>
2022-05-09 14:01:38 +0200

    1

---
+++ Calender/TradingBot/src/Person.java
@@ -0,0 +1,9 @@
+public class Person {
+    String name;
+    String nachname;
+
+    public Person (String name, String nachname) {
+        this.name = name;
+        this.nachname = nachname;
+    }
+}
