48ed02685fbf83912bd03722915f626569bdf9c8
Harant, Daniel <daniel.harant@student.fhws.de>
2022-05-26 15:24:35 +0200

    Json builder gefixt, Json parser für trades optimiert, getAccount Methode in connection

--- JavaBackend/Connection/API/Connection.java
+++ JavaBackend/Connection/API/Connection.java
@@ -41,11 +41,10 @@ public class Connection {
 		return GET(urlString + "/accounts");
 
 	}
-	
 	public String getAccount() {
-		return GET(urlString + "/accounts/" + accId);
-	}
-
+        return GET(urlString + "/accounts/" + accId);
+    }
+	
 	public String getTrades() {
 		return GET(urlString + "/accounts/" + accId + "/" + "trades");
 	}
--- JavaBackend/Connection/JsonParser/JsonBuilder.java
+++ JavaBackend/Connection/JsonParser/JsonBuilder.java
@@ -1,57 +1,86 @@
 package JsonParser;
 
+import java.util.Stack;
+
+import Exeptions.invalidJsonOperation;
+
 public class JsonBuilder {
-	
 	String json;
-	int writingObject;
-	int writingArray;
-	
-	public JsonBuilder() {
-		json = "{";
-		this.writingObject = 0;
-		this.writingArray = 0;
-	}
-	
-	public void addString(String name, String string) {
-		json += "\"" + name + "\":\"" + string +"\",";
-	}
-	
-	public void addObject(String name, JsonObject jsonObject) {
-		json += "\"" + name + "\":\"" + jsonObject.toString() +"\",";
-	}
-	
-	public void addArray(String name, JsonArray jsonArray) {
-		
-	}
-	
-	public void openObject(String name) {
-		json += "\"" + name + "\":{";
-		writingObject++;
-				
-	}
-	
-	
-	public void closeObject(String name) {
-		json += "},";
-		writingObject--;
-	}
-	
-	
-	public void openArray(String name) {
-		json+= "\"" + name + "\":[";
-		writingArray++;
-	}
-	
-	
-	public void closeArray(String name) {
-		json += "],";
-		writingArray--;
-}
-	public String build() {
-		if(writingObject == 0 && writingArray == 0) {
-			return json;
-		}
-		else  return null;
-			
-	}
+
+    Stack<Boolean> stack; // false = Object, true = Array;
+
+
+    public JsonBuilder() {
+        json = "{";
+        stack = new Stack<>();
+    }
+
+    public void addString(String name, String string) {
+        json += "\"" + name + "\":\"" + string + "\",";
+    }
+
+    public void addObject(String name, JsonObject jsonObject) {
+        json += "\"" + name + "\":\"" + jsonObject.toString() + "\",";
+    }
+
+    public void addArray(String name, JsonArray jsonArray) {
+
+    }
+
+    public void pushObject() {
+        stack.push(false);
+    }
+
+    public void pushArray() {
+        stack.push(true);
+    }
+
+    public boolean popObject() {
+        boolean output = !stack.peek();
+
+        if (output) {
+            stack.pop();
+        }
+
+        return output;
+    }
+
+    public boolean popArray() {
+        boolean output = stack.peek();
+
+        if (output) {
+            stack.pop();
+        }
+
+        return output;
+    }
+
+
+    public void openObject(String name) {
+        json += "\"" + name + "\":{";
+        pushObject();
+    }
+
+    public void closeObject(String name) {
+        if(popObject()) json += "},";
+        else throw new invalidJsonOperation("can not close Object");
+    }
+
+    public void openArray(String name) {
+        json += "\"" + name + "\":[";
+        pushArray();
+    }
+
+    public void closeArray(String name) {
+        if(popArray()) json += "],";
+        else throw new invalidJsonOperation("can not close Array");
+    }
+
+    public String build() {
+        if (stack.size() == 0) {
+            return json;
+        } else
+            throw new invalidJsonOperation("can not build Json, check if all Objects and Arrays have been closed");
+
+    }
 }
