98c48041f6ab7823b480fed97d88f27424fa61de
Heyder, Tim (BWI 4) <tim.heyder@student.fhws.de>
2022-06-07 18:54:57 +0200

    update

--- Calender/Python/Calculation.py
+++ Calender/Python/Calculation.py
@@ -1,7 +1,26 @@
+import datetime
+
+
 FACTOR_VOTALITY_HIGH = 2
 FACTOR_VOTALITY_MEDIUM = 1
+longShort = True    #true bei long, false bei short
 
+
+@staticmethod
+def DateStringToObject(word):
+    word = datetime.datetime.strptime(word, "%Y-%m-%dT%H:%M:%SZ")
+    return word
+
+@staticmethod
+def breakTimer(EventTime):
+    return EventTime - datetime.datetime.now()
 
 def calculate(event):
     actual = event["actual"]
     previous = event["previous"]
+    delta_act_prev = previous - actual
+    ratioDeviation = event["ratioDeviation"]
+    isBetterThanExpected = event["isBetterThanExpected"]
+    timedeltaFaktor = (breakTimer(DateStringToObject(event["dateUtc"]))/datetime.timedelta(hours=1))*0.5
+   
+    factor = delta_act_prev 
