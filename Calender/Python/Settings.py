6ecafa07fe22432b0e7be2c0edd92a94651eb631
Tim <tim.heyder@student.fhws.de>
2022-06-12 14:00:31 +0200

    updated instrumentLoop and upcoming

--- Calender/Python/Settings.py
+++ Calender/Python/Settings.py
@@ -1,7 +1,7 @@
-FORMAT_UPCOMING = '''"upcoming":["instrument": None,"volatility": None,"time":None]'''
+FORMAT_UPCOMIN"upcoming"G = ''':["instrument": None,"volatility": None,"time":None]'''
 
 
-FORMAT_ORDER = '''"order":["instrument": "GBP","factor": 1,"volatility": 1,"longShort": True]'''
+FORMAT_ORDER = '''"order":["instrument": "GBP","volatility": 1,"factor": 1,"longShort": True]'''
 
 
 
