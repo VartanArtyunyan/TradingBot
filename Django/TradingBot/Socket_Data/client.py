02fd40aab36608c04514b5656198dae687cfa4d9
Samuel <samuel.rajabi@student.fhws.de>
2022-06-14 14:31:35 +0200

    client file

---
+++ Django/TradingBot/Socket_Data/client.py
@@ -0,0 +1,27 @@
+import socket
+import threading
+import sys
+
+
+# Get Message
+
+def recv_msg():
+    while True:
+        recv_msg = s.recv(1024)
+        if not recv_msg:
+            sys.exit(0)
+        recv_msg = recv_msg.decode()
+        print(recv_msg)
+
+
+# Main
+
+host = 'localhost'
+port = 12001
+
+s = socket.socket()
+s.connect((host, port))
+
+print("Connected to the server")
+
+recv_msg()
