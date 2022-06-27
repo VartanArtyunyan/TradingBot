47d5399f312728f21b7006a9a561b17c29dc8a2b
Heyder, Tim (BWI 4) <tim.heyder@student.fhws.de>
2022-06-27 16:12:48 +0200

    kk

--- Calender/Python/StoreList.py
+++ Calender/Python/StoreList.py
@@ -26,11 +26,13 @@ class StoreList:
         for nextEvent in self.list_news:
             next_time =  Calculation.DateStringToObject(nextEvent["dateUtc"])
             
-            if next_time < datetime.datetime.utcnow():
+            if nextEvent["actual"] is not None:     #dürfte nicht gebraucht werden, da die Zeit sowieso verglichen wurde
                 self.list_news.remove(nextEvent)
                 continue
-            elif nextEvent["actual"] is not None:     #dürfte nicht gebraucht werden, da die Zeit sowieso verglichen wurde
+            elif next_time < datetime.datetime.utcnow():
                 self.list_news.remove(nextEvent)
+                
+            
                 
     def EventLoop(self):
         pre_string = "order"
