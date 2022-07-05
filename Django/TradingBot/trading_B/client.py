8d831509a55eeedf7309a23d89cabef31f9fa4b7
Samuel <samuel.rajabi@student.fhws.de>
2022-07-05 19:58:42 +0200

    appending file changed

--- Django/TradingBot/trading_B/client.py
+++ Django/TradingBot/trading_B/client.py
@@ -2,13 +2,8 @@
 import socket
 import threading
 import sys
-<<<<<<< HEAD:Django/TradingBot/trading_B/client.py
 import os
-=======
 
-
-# Get Message
->>>>>>> 80316a51da5074e78d880951e99383788f81f63f:Django/TradingBot/Socket_Data/client.py
 
 def recv_msg():
     while True:
@@ -17,13 +12,11 @@ def recv_msg():
             sys.exit(0)
         recv_msg = recv_msg.decode()
         print(recv_msg)
-        f = open("../jsonData/basedata.json", "w")
+        f = open("../jsonData/basedata.json", "a")
         f.write(recv_msg)
 
 
-
 # Main
-
 host = 'localhost'
 port = 12001
 
