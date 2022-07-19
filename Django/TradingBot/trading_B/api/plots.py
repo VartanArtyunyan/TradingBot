90974c73a7e768506ba00c758c74831587ffac3d
Samuel <samuel.rajabi@student.fhws.de>
2022-07-19 10:05:22 +0200

    data changes

---
+++ Django/TradingBot/trading_B/api/plots.py
@@ -0,0 +1,31 @@
+import base64
+import numpy as np
+from matplotlib import style
+import matplotlib.pyplot as plt
+from matplotlib import style
+from io import BytesIO
+
+
+def get_graph():
+    buffer = BytesIO()
+    plt.savefig(buffer, format='png')
+    buffer.seek(0)
+    image_png = buffer.getvalue()
+    graph = base64.b64encode(image_png)
+    graph = graph.decode('utf-8')
+    buffer.close()
+    return graph
+
+
+def get_plot(x, y):
+    plt.switch_backend('AGG')
+    fig, ax = plt.subplots(figsize=(10, 7))
+    ax.plot(x, y)
+    start, end = ax.get_xlim()
+    ax.set_xticks(np.arange(start, end, 4))
+    plt.gca().ticklabel_format(axis="y", useOffset=False)
+    plt.gcf().autofmt_xdate()
+    plt.plot()
+    plt.savefig('saved_figure.png')
+    graph = get_graph()
+    return graph
